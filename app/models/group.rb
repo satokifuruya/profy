faa32cb9ff99990b7721ef016a31c1fc99a57e0e
satokifuruya <Satoki@FS-no-MacBook-Air.local>
2015-12-12 12:51:02 +0900

    first commit

---
+++ app/models/group.rb
@@ -0,0 +1,6 @@
+class Group < ActiveRecord::Base
+  has_many :users
+  has_many   :questions, ->{ order("created_at DESC") }
+  has_one :feed_content, as: :content, dependent: :destroy
+  has_many :feed_contents, ->{ order("updated_at DESC") }
+end
---
+++ app/models/question.rb
@@ -0,0 +1,25 @@
+class Question < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :group
+  has_many :answers, dependent: :destroy
+  has_one :feed_content, as: :content, dependent: :destroy
+
+  validates_presence_of :user_id, :text, :group_id
+
+  # callback
+  after_create :create_feed_content
+
+  def user_answer(user_id)
+    Answer.find_by(user_id: user_id, question_id: id)
+  end
+
+  def answered?(user)
+    answers.exists?(user_id: user.id)
+  end
+
+  private
+  def create_feed_content
+    self.feed_content = FeedContent.create(group_id: group_id, created_at: created_at)
+  end
+
+end
